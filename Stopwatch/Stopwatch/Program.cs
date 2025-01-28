62a2a59f50724f1660916e26994c53c4ab4a9c28
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-28 12:00:27 -0600

    Stopwatch App

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopwatchFucntion;
+
+            string lapse = string.Empty;
+
+            bool appRuntime = true;
+            Console.WriteLine("Stopwatch Demo!\n");
+
+            // Instanciating Stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine("Welcome to Star Stopwatch\nPlease enter 'b' to 'Start', 'e' to End,\nOR 'q' to quit program\n");
+
+            stopwatchFucntion = Console.ReadLine();
+
+            do
+            {
+
+                if (stopwatchFucntion == "b")
+                {
+                    Console.WriteLine("\nStopwatch 'STARTED'\n");
+
+                    stopwatch.StartStopwatch();
+
+
+                }
+                    Console.ReadLine().ToLower();
+
+                if (stopwatchFucntion == "b")
+                {
+                    stopwatch.EndStopwatch();
+                    Console.WriteLine("\nStopwatch 'STOPPED'\n");
+                    Console.WriteLine("Stopwatch 'STARTED Again'\n");
+                    stopwatch.StartStopwatch();
+                }
+                    Console.ReadLine().ToLower();
+                
+                if (stopwatchFucntion == "b")
+                {
+                    Console.WriteLine("\n' InvalidOperationException ' Can not 'START' & 'STOP' Twice in a row");
+                    stopwatch.EndStopwatch();
+                    appRuntime = false;
+
+                }
+                    Console.ReadLine().ToLower();
+
+
+                if(stopwatchFucntion == "e")
+                {
+                    Console.WriteLine("\nStopwatch 'STOPPED'\n");
+                    stopwatch.EndStopwatch();
+
+                }
+                    Console.ReadLine().ToLower();
+
+
+                if(stopwatchFucntion == "q" || stopwatchFucntion != "e")
+                {
+                    Console.WriteLine("\nProgram Terminated\n");
+                    appRuntime = false;
+                    return;
+                }            
+            }
+            while (appRuntime);
+                    //Console.WriteLine("\nEnter '0' to Stop Stopwatch\n");
+
+
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        //public string start;
+        private DateTime _startOne;
+        private DateTime _startTwo;
+        private DateTime _end;
+        private DateTime _timeDifference;
+        //private TimeOnly timeStart;
+
+        public void StartStopwatch()
+        {
+                _startOne = DateTime.Now.ToLocalTime();
+
+                Console.WriteLine($"Stopwatch Started. And the value is:   {_startOne}");
+            
+        }
+
+
+
+        public void EndStopwatch() 
+        {
+
+            _end = DateTime.Now.ToLocalTime();
+
+            Console.WriteLine($"Stopwatch Ended. And the value is:   {_end}\n");
+            //Console.WriteLine(_end.ToString("HH:mm"));
+
+            TimeSpan _timeDifference = _end - _startOne;
+
+            Console.WriteLine($"\nThe Time Difference is {_timeDifference}");
+        }
+
+
+        public void ClearStopwatch()
+        {
+            _startOne = DateTime.MinValue;
+            _end = DateTime.MinValue;
+        }
+
+    }
+}
