6b8b9177cb9647270706ee5b074765d2ddf38a2c
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-28 12:15:32 -0600

    Initial commit for StackOverflowPost app

---
+++ StackOverflowPost/StackOverflowPost/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    internal class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Vote { get; private set; }
+        public DateTime VoteTime { get; set; }
+
+        
+        public void UpVote()
+        {
+            VoteTime = DateTime.Now;
+        }
+
+
+    }
+}
